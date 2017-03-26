980bdc838315a5cd237e5a4087140837486da7db
Mura, William <william.mura@nuance.com>
2017-03-25 23:54:00 -0400

    Adding soustract function to timer service

--- src/services/timer.js
+++ src/services/timer.js
@@ -6,14 +6,17 @@ var TimerService = {
         this.stop = new Date();
     },
     getDifference: function() {
-        var time = (this.stop.getTime() - this.start.getTime()) / 1000
-        return time;
+        var time = (this.stop.getTime() - this.start.getTime()) / 1000 // In seconds
+        return time * 60; // In minutes
     },
     setRemainingTime: function(value) {
         this.remainingTime = value;
     },
     getRemainingTime: function() {
         return this.remainingTime;
+    },
+    soustract: function(value) {
+        this.remainingTime -= value;
     }
 };
 
